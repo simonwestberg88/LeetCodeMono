00fe2e68a974fc04e73c7d3f612cf866f168b4c6
simon westberg <simon.westberg88@gmail.com>
2022-06-19 19:46:58 +0200

    tosum solution

---
+++ TwoSum/TwoSum/Solution.cs
@@ -0,0 +1,20 @@
+﻿public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        var dictionary = new Dictionary<int, int>();
+        var index = 0;
+        var index2 = 0;
+        foreach (int num in nums)
+        {
+            var otherNum = target - num;
+            if (dictionary.TryGetValue(otherNum, out index2))
+            {
+                return new int[] {index2, index};
+            }
+            dictionary.TryAdd(num, index);
+
+            index++;
+        }
+        return new int[] {1,1};
+    }
+    
+}
