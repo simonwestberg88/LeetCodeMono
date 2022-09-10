228b646eb5aa566860148d24bee6141302c23d51
simon westberg <simon.westberg88@gmail.com>
2022-09-10 09:47:33 +0200

    simple calculations

--- FindMedianSortedArrays/Program.cs
+++ FindMedianSortedArrays/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FindMedianSortedArrays;
+
+var l1 = new [] {1,2,7};
+var l2 = new [] {3,4,5,6};
+
+
+decimal x = (decimal)(1+2)/2;
+
+
+
+var solution = new Solution();
+var ans = solution.FindMedianSortedArrays(l1,l2);
+ Console.WriteLine($"ans: {ans}");
--- FindMedianSortedArrays/Solution.cs
+++ FindMedianSortedArrays/Solution.cs
@@ -2,5 +2,31 @@ namespace FindMedianSortedArrays;
 
 public class Solution
 {
-    
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+
+        //get length of both arrays
+        //get median of both arrays
+        // if median of longer 
+        var totalLength = nums1.Length + nums2.Length;
+
+
+
+
+        return 0;
+    }
+
+    decimal GetMedian(int[] nums)
+    {
+        var length = nums.Length;
+        decimal median = nums[length/2];
+        if (IsEven(nums.Count()))
+        {
+            var i = nums.Count() / 2;
+            median = (decimal)(nums[i - 1] + nums[i]) / 2;
+        }
+        return median;
+    }
+
+    bool IsEven(int num) => num % 2 == 0;
 }
