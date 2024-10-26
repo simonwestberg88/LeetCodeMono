dc297bfbf1a2e3854b8a1f0f33b7efd5cf29da77
simonwestberg88 <simon.westberg88@gmail.com>
2024-10-26 18:23:21 +0200

    start of reverse

---
+++ Leetcode/ReverseInteger/Program.cs
@@ -0,0 +1,20 @@
+﻿// Example 1:
+//
+// Input: x = 123
+// Output: 321
+// Example 2:
+//
+// Input: x = -123
+// Output: -321
+// Example 3:
+//
+// Input: x = 120
+// Output: 21
+
+using ReverseInteger;
+
+var sln = new Solution();
+
+var test1 = sln.Reverse(321);
+
+Console.WriteLine(test1);
---
+++ Leetcode/ReverseInteger/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace ReverseInteger;
+
+public class Solution
+{
+    public int Reverse(int x)
+    {
+        if (x == 0) return 0;
+        if (x == 1) return 1;
+        var rList = new List<int>();
+        for (var i = 0; i < 9; i++)
+        {
+            var digit = x % 10;
+            rList.Add(digit);
+            x /= 10;
+            if (x == 0) break;
+        }
+
+        var newInt = 0;
+        var mult = 1;
+        for (var j = rList.Count -1; j >= 0; j--)
+        {
+            newInt += rList[j] * mult;
+            mult *= 10;
+        }
+        return newInt;
+    }
+}
