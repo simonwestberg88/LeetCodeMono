56c096ab0c59d9cc1a4fedcfd8c710f7132384de
simonwestberg88 <simon.westberg88@gmail.com>
2024-10-29 21:44:53 +0100

    palindrome dun

---
+++ Leetcode/PalindromeNumber/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace PalindromeNumber;
+
+public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0) return false;
+        var numString = x.ToString();
+        var middle = numString.Length / 2;
+
+        for (var i = 0; i < numString.Length / 2; i++)
+        {
+            var a = numString[i];
+            var b = numString[^(i+1)];
+            if (a != b)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
