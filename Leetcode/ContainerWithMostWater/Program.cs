aeaea247964dd845bce3dd7b6bb6cb440d9db1c7
simonwestberg88 <simon.westberg88@gmail.com>
2024-10-31 16:37:54 +0100

    solved but too slow

---
+++ Leetcode/ContainerWithMostWater/Program.cs
@@ -0,0 +1,7 @@
+﻿using ContainerWithMostWater;
+
+var sln = new Solution();
+var t1 = sln.MaxArea([1,8,6,2,5,4,8,3,7]);
+var t2 = sln.MaxArea([1,1]);
+
+Console.WriteLine("");
---
+++ Leetcode/ContainerWithMostWater/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace ContainerWithMostWater;
+
+public class Solution
+{
+    public int MaxArea(int[] height)
+    {
+        var max = 0;
+        for (var i = 0; i < height.Length; i++)
+        {
+            var a = height[i];
+            for (var j = 1; j < height.Length; j++)
+            {
+                var b = height[^j];
+                var lowest = int.Min(a, b);
+                var length = height.Length - j - i;
+                var area = lowest * length;
+                if (area > max)
+                {
+                    max = area;
+                }
+            }
+        }
+
+        return max;
+    }
+}
