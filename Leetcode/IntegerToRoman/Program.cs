9881ff019bbc1d992b56fb073c689435d724bb06
simonwestberg88 <simon.westberg88@gmail.com>
2024-11-04 20:39:42 +0100

    int to roman done

---
+++ Leetcode/IntegerToRoman/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using IntegerToRoman;
+
+var sln = new Solution();
+
+// var t1 = sln.IntToRoman(1);
+// var t2 = sln.IntToRoman(2);
+// var t3 = sln.IntToRoman(3);
+// var t4 = sln.IntToRoman(4);
+// var t5 = sln.IntToRoman(5);
+var t9 = sln.IntToRoman(1994);
---
+++ Leetcode/IntegerToRoman/Solution.cs
@@ -0,0 +1,66 @@
+﻿namespace IntegerToRoman;
+
+public class Solution
+{
+    public string IntToRoman(int num)
+    {
+        var dict = new Dictionary<int, string>();
+        dict[1] = "I";
+        dict[4] = "IV";
+        dict[5] = "V";
+        dict[9] = "IX";
+        dict[10] = "X";
+        dict[40] = "XL";
+        dict[50] = "L";
+        dict[90] = "XC";
+        dict[100] = "C";
+        dict[400] = "CD";
+        dict[500] = "D";
+        dict[900] = "CM";
+        dict[1000] = "M";
+        var s = new Stack<string>();
+        for (var i = 0; i < 4; i++)
+        {
+            if (num == 0) break;
+            var mod = num % 10;
+            num = num / 10;
+            var mult = (int)Math.Pow(10, i);
+            if (mod is 4 or 9 or 5)
+            {
+                s.Push(dict[mod * mult]);
+                continue;
+            }
+
+            if (mod == 0)
+            {
+                continue;
+            }
+
+            if (mod < 4)
+            {
+                for (var j = 0; j < mod; j++)
+                {
+                    s.Push(dict[mult]);
+                }
+
+                continue;
+            }
+
+            var mod5 = mod % 5;
+            var t = mult * 5;
+            for (var k = 0; k < mod5; k++)
+            {
+                s.Push(dict[mult]);
+            }
+            s.Push(dict[t]);
+        }
+
+        var ret = "";
+        while(s.TryPop(out var c))
+        {
+            ret += c;
+        }
+
+        return ret;
+    }
+}
