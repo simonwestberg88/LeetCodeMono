31ec040b22a1c9e3c488f34c717560f8b93f3212
simonwestberg88 <simon.westberg88@gmail.com>
2024-11-05 21:02:31 +0100

    longest common done

---
+++ Leetcode/LongestCommonPrefix/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LongestCommonPrefix;
+
+var sln = new Solution();
+var strs = new []{"flower", "flow", "flight"};
+var t1 = sln.LongestCommonPrefix(strs);
+var t2 = sln.LongestCommonPrefix(["ab", "a"]);
+
+Console.WriteLine("Hello, World!");
---
+++ Leetcode/LongestCommonPrefix/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LongestCommonPrefix;
+
+public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        if (strs.Length == 1) return strs[0];
+        var longest = strs[0];
+        for (var i = 1; i < strs.Length; i++)
+        {
+            if (strs[i].Length == 0) return "";
+            var len = Math.Min(longest.Length, strs[i].Length);
+            var n = "";
+            for (var j = 0; j < len; j++)
+            {
+                if (longest[j] != strs[i][j])
+                {
+                    if (n.Length == 0) return "";
+                    longest = n;
+                    break;
+                }
+
+                n += longest[j];
+            }
+
+            longest = n;
+        }
+
+        return longest;
+    }
+}
