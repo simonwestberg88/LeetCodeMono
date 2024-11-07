430acde2efa8fbcf410e8a5805b071fc5e47640e
simonwestberg88 <simon.westberg88@gmail.com>
2024-11-07 20:44:12 +0100

    time limit issue

---
+++ Leetcode/ThreeSum/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ThreeSum;
+
+var sln = new Solution();
+var t1 = sln.ThreeSum([-1, 0, 1, 2, -1, -4]);
+
+Console.WriteLine("Hello, World!");
---
+++ Leetcode/ThreeSum/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+namespace ThreeSum;
+
+public class Solution
+{
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        var hashSet = new HashSet<(int, int, int)>();
+        var ret = new List<IList<int>>();
+        for (var i = 0; i < nums.Length - 2; i++)
+        {
+            for (var j = i + 1; j < nums.Length - 1; j++)
+            {
+                for (var k = j + 1; k < nums.Length; k++)
+                {
+                    if (nums[i] + nums[j] + nums[k] != 0) continue;
+                    var list = new List<int> { nums[i], nums[j], nums[k] };
+                    list.Sort();
+                    hashSet.Add((list[0], list[1], list[2]));
+                }
+            }
+        }
+
+        foreach (var hash in hashSet)
+        {
+            ret.Add(new List<int>{hash.Item1, hash.Item2, hash.Item3});
+        }
+
+        return ret;
+    }
+}
